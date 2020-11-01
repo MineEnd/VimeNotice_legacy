71c5012cc701f21739c467e1ad84280a3462e156
MineEnd <vilesovilya@gmail.com>
2020-11-01 22:02:09 +0700

    Alpha 1.4

--- VimeNotice/APIKey.cs
+++ VimeNotice/APIKey.cs
@@ -23,7 +23,7 @@ namespace VimeNotice
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            textBox1.Text
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
